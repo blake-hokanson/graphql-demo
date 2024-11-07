b31696953893007ad986a02ecd9a3a56098ed095
blake-hokanson <blake.hokanson@icloud.com>
2024-11-06 18:42:25 -0600

    added link type

--- src/index.js
+++ src/index.js
@@ -3,13 +3,32 @@ const {ApolloServer} = require('apollo-server');
 const typeDefs = `
   type Query {
     info: String!
+    feed: [Link]!
+  }
+  
+  type Link {
+    id: ID!
+    description: String!
+    url: String!
   }
 `
 
+const links = [{
+  id: 'link-0',
+  description: 'this is a link',
+  url: 'www.example.com',
+}]
+
 const resolvers = {
   Query: {
-    info: () => 'this is a string'
-  }
+    info: () => 'this is a string',
+    feed: () => links,
+  },
+  Link: {
+    id: (parent) => parent.id,
+    description: (parent) => parent.description,
+    url: (parent) => parent.url,
+  },
 }
 
 const server = new ApolloServer({
