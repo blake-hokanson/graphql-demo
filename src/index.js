94e95531e098bd4fe03c35a5c2e0db6849cdc2f7
blake-hokanson <blake.hokanson@icloud.com>
2024-11-12 14:33:42 -0600

    added filter

--- src/index.js
+++ src/index.js
@@ -3,16 +3,27 @@ const fs = require('fs')
 
 const {ApolloServer} = require('apollo-server');
 
-const links = [{
-  id: 'link-0',
-  description: 'this is a link',
-  url: 'www.example.com',
-}]
+const links = [
+  {
+    id: 'link-0',
+    description: 'this is a link',
+    url: 'www.example.com',
+  },{
+    id: 'link-1',
+    description: 'this is also a link',
+    url: 'www.google.com',
+  }
+  ]
 
 const resolvers = {
   Query: {
     info: () => 'this is a string',
-    feed: () => links,
+    feed: (parent, args) => {
+      if (args.filter_id) {
+        return links.filter(link => link.id === args.filter_id);
+      }
+      return links;
+    },
   },
   Mutation: {
     post: (parent, args) => {
